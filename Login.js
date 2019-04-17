3b6bb9baa60e0136d9e654ee6c9a421a07b02cf9
EJoling <emieljoling@gmail.com>
2019-04-17 18:11:26 +0200

    pull en merge testje

--- Login.js
+++ Login.js
@@ -21,7 +21,7 @@ function POSTTrainee(currentUser){
       console.log(user);
       sessionStorage.setItem("storedUserID", user.id);
       console.log(user.id);
-      // window.location.href = 'KlantHomepage.html';
+      //window.location.href = 'KlantHomepage.html';
       window.location.href = 'TraineeUrenoverzicht.html';
     }
   };
