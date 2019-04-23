37cf4df52bf9d9e416e1d97035a9069d7d373c78
EJoling <emieljoling@gmail.com>
2019-04-23 17:56:43 +0200

    lege commit

--- KlantHomepage.js
+++ KlantHomepage.js
@@ -10,7 +10,7 @@ var klant;
 function GETUrenPerTrainee(){
   console.log ("huidige ingelogde user:")
   console.log(sessionStorage.getItem("storedUserID"));
-  var table = document. getElementById("traineelijst");
+  //var table = document. getElementById("traineelijst");
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
